cb0eb3548765c08da93066cb7b2d22462d7b2708
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-06-06 00:52:35 +0300

    add project structure, mooki, reset scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import ProductCard from "./components/ProductCard.tsx";
-
 function App() {
-
   return (
     <>
       <h1>Hello</h1>
-        <ProductCard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Catalog.tsx
+++
@@ -1,3 +0,0 @@
-export const ProductCard = () => {
-  return <div>Product</div>;
-};
