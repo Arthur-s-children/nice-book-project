4ac59a65535644d968dd23eb26736d32d863dd21
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-06-08 01:25:23 +0300

    add hero section and decomposition for HomePage

---
+++ src/components/shared/HeroSection/HeroSlides.ts
@@ -0,0 +1,29 @@
+import desktop1 from '../../../assets/img/heroSlides/tetsbanner1.webp';
+import desktop2 from '../../../assets/img/heroSlides/testbanner22.webp';
+import desktop3 from '../../../assets/img/heroSlides/testbanner3.webp';
+
+import tablet1 from '../../../assets/img/heroSlides/bannerTablet1.webp';
+import tablet2 from '../../../assets/img/heroSlides/bannerTablet2.webp';
+import tablet3 from '../../../assets/img/heroSlides/bannerTablet3.webp';
+
+import mobile1 from '../../../assets/img/heroSlides/mobileBanner1.webp';
+import mobile2 from '../../../assets/img/heroSlides/mobileBanner2.webp';
+import mobile3 from '../../../assets/img/heroSlides/mobileBanner3.webp';
+
+export const heroSlides = [
+  {
+    desktop: desktop1,
+    tablet: tablet1,
+    mobile: mobile1,
+  },
+  {
+    desktop: desktop2,
+    tablet: tablet2,
+    mobile: mobile2,
+  },
+  {
+    desktop: desktop3,
+    tablet: tablet3,
+    mobile: mobile3,
+  },
+];
