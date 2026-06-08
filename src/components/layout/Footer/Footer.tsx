6766c19464bfa8efee5cb419d9d472003246a709
Yaroslav <gaubetsa@gmail.com>
2026-06-08 13:40:15 +0300

    fixed foter path

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,8 +1,53 @@
-const Footer = () => {
+import './Footer.scss';
+import { Logo } from '../../Logo';
+import { Icon } from '../../Icon';
+
+const Footer: React.FC = () => {
+  const handleBackToTop = (): void => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <div>
-      <h2>FOOTER</h2>
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <a
+          href="/"
+          className="logo"
+        >
+          <Logo />
+        </a>
+
+        <nav className="categories">
+          <a
+            className="category"
+            href="#github"
+          >
+            GITHUB
+          </a>
+          <a
+            className="category"
+            href="#contacts"
+          >
+            CONTACTS
+          </a>
+          <a
+            className="category"
+            href="#rights"
+          >
+            RIGHTS
+          </a>
+        </nav>
+
+        <button
+          type="button"
+          className="backTop"
+          onClick={handleBackToTop}
+        >
+          Back to top
+          <Icon name="arrow-up" />
+        </button>
+      </div>
+    </footer>
   );
 };
 
