61c58fe2ab979c40558f59aae98448df64dfb33d
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-06-08 21:24:27 +0300

    customize swiper (hero banners)  pagination and arrows

--- src/components/shared/HeroSection/HeroSection.tsx
+++ src/components/shared/HeroSection/HeroSection.tsx
@@ -2,21 +2,32 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import { heroSlides } from './HeroSlides.ts';
+import { heroSlides } from './HeroSlides';
+import { Icon } from '../../Icon';
 
 import './HeroSection.scss';
 
 export const HeroSection = () => {
   return (
     <section className="hero">
+      <button
+        className="hero__button hero__button--prev"
+        type="button"
+      >
+        <Icon name="arrow-left" />
+      </button>
+
       <Swiper
         modules={[Navigation, Pagination]}
-        navigation
+        navigation={{
+          prevEl: '.hero__button--prev',
+          nextEl: '.hero__button--next',
+        }}
         pagination={{ clickable: true }}
         loop
+        className="hero__swiper"
       >
         {heroSlides.map((slide, index) => (
           <SwiperSlide key={index}>
@@ -40,6 +51,13 @@ export const HeroSection = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <button
+        className="hero__button hero__button--next"
+        type="button"
+      >
+        <Icon name="arrow-right" />
+      </button>
     </section>
   );
 };
